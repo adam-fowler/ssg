626effb0af5c9bf52f6e0b4ba95d7a49dffd647e
Adam Fowler <adamfowler71@gmail.com>
2020-01-09 17:33:04 +0100

    Although user to choose the shell they run

--- Sources/ssg/cli.swift
+++ Sources/ssg/cli.swift
@@ -1,8 +1,8 @@
 import Foundation
 
-@discardableResult public func shell(_ args: String...) -> Int32 {
+@discardableResult public func shell(shell: String = "/bin/bash/", _ args: String...) -> Int32 {
     let task = Process()
-    task.launchPath = "/bin/bash/"
+    task.launchPath = shell
     task.arguments = args
     task.launch()
     task.waitUntilExit()
