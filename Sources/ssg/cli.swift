7e9acbcf7518389a9237c346c83737274133002c
Adam Fowler <adamfowler71@gmail.com>
2025-01-30 14:16:20 +0000

    Added updateShellEnvironmentPath

--- Sources/ssg/cli.swift
+++ Sources/ssg/cli.swift
@@ -8,3 +8,23 @@ import Foundation
     task.waitUntilExit()
     return task.terminationStatus
 }
+
+/// Update the PATH environment variable to be the same as the shell that
+/// triggered the app
+public func updateShellEnvironmentPath() {
+    let taskShell = Process()
+    taskShell.launchPath = "/usr/bin/env"
+    let shell = ProcessInfo.processInfo.environment["SHELL"]!
+    taskShell.arguments = [shell, "-c", "eval $(/usr/libexec/path_helper -s) ; echo $PATH"]
+    let pipeShell = Pipe()
+    taskShell.standardOutput = pipeShell
+    taskShell.standardError = pipeShell
+    taskShell.launch()
+    taskShell.waitUntilExit()
+    let output = pipeShell.fileHandleForReading.readDataToEndOfFile()
+    var outputShell = String(decoding: output, as: UTF8.self)
+    outputShell = outputShell.replacingOccurrences(
+        of: "\n", with: "", options: .literal, range: nil)
+    print(outputShell)
+    setenv("PATH", outputShell, 1)
+}
--- Tests/ssgTests/ssgTests.swift
+++ Tests/ssgTests/ssgTests.swift
@@ -1,15 +1,10 @@
 import XCTest
+
 @testable import ssg
 
 final class ssgTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        //XCTAssertEqual(ssg().text, "Hello, World!")
+    func testShell() {
+        shell("swift", "--version")
+        shell("magick")
     }
-
-    static var allTests = [
-        ("testExample", testExample),
-    ]
 }
